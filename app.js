4f0a02275a9853a5fb97a1d946478299bc208fdd
Nicole Sattler <nicole.sattler@live.com>
2017-11-01 23:02:17 +0000

    Modified to render 404-error html file

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ var whoAmIRoute = require("./routes/whoami"),
  
 app.use(express.static(__dirname+"/public")); 
 app.set("view engine", "ejs");
+app.engine('html', require('ejs').renderFile);
 
 app.use(indexRoute);
 app.use(whoAmIRoute); 
 app.use(prettyfiedRoute);
 
 app.use(function(req, res, next){
-    res.status(404).render('404-error', {title: "Page not found"});
+    res.status(404).render('404-error.html');
 });
 
 app.listen(process.env.PORT, function(){
